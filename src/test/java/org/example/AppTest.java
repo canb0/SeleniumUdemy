c21c172bf68449fe89fb31ac9a1af3bc2ad05136
canberkakduygu <canberk.akduygu@ah.nl>
2023-11-18 14:00:12 +0300

    first commit

---
+++ src/test/java/org/example/AppTest.java
@@ -0,0 +1,58 @@
+package org.example;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxBinary;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.testng.Assert;
+import org.testng.annotations.*;
+
+
+public class AppTest{
+
+    //-----------------------------------Global Variables-----------------------------------
+    //Declare a Webdriver variable
+    public WebDriver driver;
+    //Declare a test URL variable
+    public String testURL;//= "http://www.swtestacademy.com/";
+    public String browser;
+
+    @BeforeMethod
+    public void setupTest (){
+        browser = System.getProperty("browser");
+        testURL = System.getProperty("url");
+
+        //Print console that code is in @BeforeMethod!
+        System.out.println("@BeforeMethod has started.");
+
+        if(browser.contains("Chrome")){
+            ChromeOptions options = new ChromeOptions();
+            options.addArguments("--headless=new");
+            driver = new ChromeDriver(options);
+        }else if (browser.contains("FireFox")) {
+            FirefoxOptions options = new FirefoxOptions();
+            options.addArguments("--headless");
+            driver = new FirefoxDriver(options);
+        }
+    }
+    //-----------------------------------Tests-----------------------------------
+    @Test
+    public void firstTest () {
+        System.out.println("First Test is Started.");
+        //Go to www.swtestacademy.com
+        driver.navigate().to(testURL);
+        //Get page title
+        String title = driver.getTitle();
+        //Print page's title
+        System.out.println("Page Title: " + title);
+        Assert.assertTrue(title.contains("Academy"));
+    }
+
+    @AfterMethod
+    public void teardownTest (){
+        System.out.println("@AfterMethod has started.");
+        driver.quit();
+    }
+}
